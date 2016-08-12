98ecc70f76d10d252c9414feb3359e890287251e
Rhett Thompson <rhett.thompson@live.com>
2016-08-12 14:12:19 -0400

    updates

---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web;
+using webmetal;
+
+namespace WebApplication1
+{
+    public class Global : WebMetalApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            init(typeof(Home), new string[] { HttpContext.Current.Server.MapPath("~/templates") });
+
+            using (var db = new LiteDB.LiteDatabase(""))
+            {
+
+            }
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/code/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using webmetal;
+
+namespace WebApplication1
+{
+    public class Home : Page
+    {
+        public override void render()
+        {
+            response.Write(view("home"));
+        }
+    }
+}
